48e538e4ec7192cf7c7331793c4077a995f1b71f
Cat-celestial <kirillkoten@gmail.com>
2023-12-26 00:02:21 +0200

    урок 3 С1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,60 @@
-let incomeInp = document.querySelector(".incomeInput");
-incomeInp.focus();
-let fitOutput = document.querySelector(".fitSum");
+let inpt = document.querySelector("#inp");
 
-let monyFiltsList = [609350, 243725, 191950, 100525, 47150, 11600, 0];
-let ratesList = [0.37, 0.35, 0.32, 0.24, 0.22, 0.12, 0.1];
+let mark = 0;
+let answerCounter = 0;
+let answNumb = 1;
+let rightAnsws = {};
+let wrongAnsws = {};
 
-function changeFIT() {
-    let monyForTax = incomeInp.value - 14600;
-    
-    if (monyForTax > 0) {
-        let fit = 0;
-        for (let i = 0; i <= monyFiltsList.length; i++) {
-            let nowRate = monyFiltsList[i];
+let realAnsws = [];
 
-            if (monyForTax >= monyFiltsList[i]) {
-                let diff = (monyForTax - nowRate) * ratesList[i];
-                fit += diff;
-                console.log(ratesList[i], diff, fit);
-                monyForTax = nowRate;
-            }
-        }
-        fitOutput.textContent = `$ ${fit.toFixed(2)}`
-    } else {
-        fitOutput.textContent = "$ 0.00"
+inpt.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("butt").click();
     }
+});
+
+function mainFunc() {
+    inpt.focus();
+    let userAnsw = inpt.value;
+
+    if (answerCounter < 12) {
+        if (inpt.value != "") {
+            inpt.setAttribute("placeholder", `Введите ответ на задачу ${answNumb + 1}`);
+            inpt.value = "";
+            inpt.focus();
+            
+            if (userAnsw == realAnsws[answerCounter]) {
+                rightAnsws[answerCounter + 1] = userAnsw;
+                mark += 1;
+            } else {
+                wrongAnsws[answerCounter + 1] = userAnsw;
+            }
+            
+            answNumb += 1;
+            answerCounter += 1;
+        }
+
+    }
+
+    if (answerCounter == 12) {
+        console.log(`Оценка: ${mark}`);
+
+        console.log("Правильные ответы: ");
+        console.log(rightAnsws);
+
+        console.log("Не правильные ответы: ");
+        console.log(wrongAnsws);
+
+    }
+
+}
+
+for (let i = 0; i < 12; i++) {
+    let firstNum = parseInt(_.shuffle(_.range(5, 10)).slice(0, 1));
+    let secondNum = parseInt(_.shuffle(_.range(1, 5)).slice(0, 1));
+    realAnsws.push(firstNum * secondNum);
+
+    console.log(`${i + 1}) ${firstNum} * ${secondNum} = `);
 }
