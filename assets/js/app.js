930f315458cf8a1d771719ec425fa292f5ee31d7
Cat-celestial <kirillkoten@gmail.com>
2023-12-26 20:58:41 +0200

    урок 3

--- assets/js/app.js
+++ assets/js/app.js
@@ -58,3 +58,22 @@ for (let i = 0; i < 12; i++) {
 
     console.log(`${i + 1}) ${firstNum} * ${secondNum} = `);
 }
+
+let creditSum = +prompt("Введите суму кредита");
+// let creditSumInUse = creditSum;
+let annualRate = +prompt("Введите годовой процент");
+let monthLoanTerm = +prompt("Введите срок кредита в месяцах");
+let monthPaym = (creditSum / monthLoanTerm).toFixed(2);
+
+let overPaym = 0;
+
+for (let i = 0; i < monthLoanTerm; i++) {
+    let paymLeft = creditSum - i * monthPaym;
+    // creditSumInUse = paymLeft;
+
+    let ratePaym = paymLeft * (annualRate / 12 / 100);
+    overPaym += ratePaym;
+    console.log(`Задолженность по кредиту: ${(paymLeft).toFixed(2)}, Погашение кредита: ${monthPaym}, Проценты по кредиту: ${(ratePaym).toFixed(2)}, Общая выплата в месяц: ${(paymLeft + ratePaym).toFixed(2)}`);
+}
+
+console.log("Переплата по кредиту: ", (overPaym).toFixed(2));
