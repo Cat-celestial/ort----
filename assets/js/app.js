83a23185e60a124d978335431256b9f949ff423f
Cat-celestial <kirillkoten@gmail.com>
2024-01-02 19:30:02 +0200

    урок 4 доработка

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,22 +2,23 @@ let inpt = document.querySelector("#inp");
 inpt.focus();
 let labelBillAm = document.querySelector("#billAmLabel");
 let labelBills = document.querySelector("#billsLabel");
+let bLab = document.querySelector("#showBills");
 
 let billsList = [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
+let billsInATM = {
+    1000: 0,
+    500: 0,
+    200: 0,
+    100: 10,
+    50: 0,
+    20: 0,
+    10: 0,
+    5: 0,
+    2: 0,
+    1: 45
+};
 
 function mainFunc() {
-    let billsInATM = {
-        1000: 5,
-        500: 1,
-        200: 2,
-        100: 10,
-        50: 1,
-        20: 2,
-        10: 6,
-        5: 0,
-        2: 2,
-        1: 5
-    };
     let sum = parseInt(inpt.value);
     console.clear();
     let billsAm = 0;
@@ -40,11 +41,18 @@ function mainFunc() {
             }
         }
     }
+    if (sum != 0) {
+        labelBillAm.textContent = `Количество купюр: в банкомате на хватает купюр`;
+        labelBills.textContent = `Купюры: `;
 
-    labelBillAm.textContent = `Количество купюр: ${billsAm}`;
-    labelBills.textContent = `Купюры: ${billsNeedList.join(" ")}`;
-    console.log(sum);
+    } else if (billsAm >= 40) {
+        labelBillAm.textContent = `Количество купюр: банкомат не выдает больше чем 40 купюр за раз`;
+        labelBills.textContent = `Купюры: `;
 
-    console.log(billsAm);
-    console.log(billsNeedList);
+    } else if (sum == 0) {
+        labelBillAm.textContent = `Количество купюр: ${billsAm}`;
+        labelBills.textContent = `Купюры: ${billsNeedList.join("; ")}`;
+    }
+
+    console.log(billsInATM);
 }
