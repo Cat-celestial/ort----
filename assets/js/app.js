127e3548b32fef7b77761c522d5a1e754fb5d9b4
Cat-celestial <kirillkoten@gmail.com>
2024-01-04 14:14:11 +0200

    урок 4 подправленный

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,23 +6,25 @@ let bLab = document.querySelector("#showBills");
 
 let billsList = [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
 let billsInATM = {
-    1000: 0,
+    1000: 3,
     500: 0,
-    200: 0,
+    200: 2,
     100: 10,
-    50: 0,
-    20: 0,
-    10: 0,
-    5: 0,
+    50: 1,
+    20: 4,
+    10: 6,
+    5: 1,
     2: 0,
     1: 45
 };
+
 
 function mainFunc() {
     let sum = parseInt(inpt.value);
     console.clear();
     let billsAm = 0;
     let billsNeedList = [];
+    let everyBillAm = [];
 
     for (let bankNote of billsList) {
         // console.log("Банкнота: ", bankNote);
@@ -30,15 +32,32 @@ function mainFunc() {
             let bNNeed = Math.floor(sum / bankNote);
             // console.log("Банкнот ", bankNote, " нужно ", bNNeed);
 
-            for (let am = billsInATM[bankNote]; am > 0; am--) {
-                if (bNNeed > 0) {
-                    billsNeedList.push(bankNote);
-                    bNNeed -= 1;
-                    billsAm += 1;
-                    sum -= bankNote;
-                    billsInATM[bankNote] -= 1;
-                }
+            if (bNNeed >= billsInATM[bankNote]) {
+                billsNeedList.push(bankNote);
+                everyBillAm.push(billsInATM[bankNote]);
+
+                billsAm += billsInATM[bankNote];
+                sum = sum - (billsInATM[bankNote] * bankNote);
+                billsInATM[bankNote] = 0;
+
+            } else if (bNNeed < billsInATM[bankNote]) {
+                billsNeedList.push(bankNote);
+                everyBillAm.push(bNNeed);
+
+                billsAm += bNNeed;
+                sum = sum - (bNNeed * bankNote);
+                billsInATM[bankNote] -= bNNeed;
             }
+
+            // for (let am = billsInATM[bankNote]; am > 0; am--) {
+            //     if (bNNeed > 0) {
+            //         billsNeedList.push(bankNote);
+            //         bNNeed -= 1;
+            //         billsAm += 1;
+            //         sum -= bankNote;
+            //         billsInATM[bankNote] -= 1;
+            //     }
+            // }
         }
     }
     if (sum != 0) {
@@ -51,7 +70,7 @@ function mainFunc() {
 
     } else if (sum == 0) {
         labelBillAm.textContent = `Количество купюр: ${billsAm}`;
-        labelBills.textContent = `Купюры: ${billsNeedList.join("; ")}`;
+        labelBills.textContent = `Купюры: ${billsNeedList.map((bl, index) => `${bl} x ${everyBillAm[index]}`).join("; ")}`;
     }
 
     console.log(billsInATM);
