24df0f7ae472ccec01c86d7231d705f2ca062924
Cat-celestial <kirillkoten@gmail.com>
2024-01-08 15:36:28 +0200

    урок 6 поправки

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,45 +9,35 @@ let yearsLab = document.querySelector("#yearsLab");
 let months = ["Января", "Февраля", "Марта", "Апреля", "Мая",
     "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"];
 
-let daysOfWeek = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Субота", "Воскресенье"]
-
-// let dt = new Date("1899-12-31");
-// dt.setDate(dt.getDate() + 34634);
-
-// let fullBirthDate = new Date(dt.getFullYear(), dt.getMonth(), dt.getDate());
-// console.log(fullBirthDate.getTime());
-
-var date1 = new Date('12-09-2017');
-var date2 = new Date('12-10-2017');
-var daysLag = Math.ceil(Math.abs(date2.getTime() - date1.getTime()) / (1000 * 3600 * 24));
-console.log(daysLag)
-
-
+let daysOfWeek = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Субота", "Воскресенье"]  
 
 function mainFunc() {
     let inn = String(mainInp.value);
     let innList = mainInp.value.split("").map((item) => +item).reverse();
 
     if (innList.length == 10) {
-        innValidityLab.textContent = "ИНН корректен";
+        innValidityLab.innerHTML = `ИНН <span class="green">корректен</span>`;
+        let gender = (num) => (num % 2 === 0) ? "Пол: женский" : "Пол: мужской";
+        genderLab.textContent = gender(innList[0]);
+
+        let birthDays = parseInt(inn.substring(0, 5));
+        let dt = new Date("1899-12-31");
+        dt.setDate(dt.getDate() + birthDays);
+
+        birthDayLab.textContent = `Дата рождения: ${dt.getUTCDate()} ${months[dt.getMonth()]} ${dt.getFullYear()}, ${daysOfWeek[dt.getDay() - 1]}`;
+
+        let fullBirthDate = new Date(dt.getFullYear(), dt.getMonth(), dt.getDate());
+        let nowadays = new Date();
+        // let nowadays = new Date(nowadaysString.getFullYear(), nowadaysString.getMonth(), nowadaysString.getDate());
+
+        let yearOld = Math.floor(Math.ceil(Math.abs(nowadays.getTime() - fullBirthDate.getTime()) / (1000 * 3600 * 24)) / 365);
+
+        yearsLab.textContent = `Полных лет человеку: ${yearOld}`;
     } else {
-        innValidityLab.textContent = "ИНН НЕ корректен";
+        innValidityLab.innerHTML = `ИНН <span class="red">НЕ</span> корректен`;
+        birthDayLab.textContent = "Дата рождения: ";
+        genderLab.textContent = "Пол: ";
+        yearsLab.textContent = "Полных лет человеку: ";
     }
 
-    let gender = (num) => (num % 2 === 0) ? "Пол: женский" : "Пол: мужской";
-    genderLab.textContent = gender(innList[0]);
-
-    let birthDays = parseInt(inn.substring(0, 5));
-    let dt = new Date("1899-12-31");
-    dt.setDate(dt.getDate() + birthDays);
-
-    birthDayLab.textContent = `Дата рождения: ${dt.getUTCDate()} ${months[dt.getMonth()]} ${dt.getFullYear()}, ${daysOfWeek[dt.getDay() - 1]}`;
-
-    let fullBirthDate = new Date(dt.getFullYear(), dt.getMonth(), dt.getDate());
-    let nowadays = new Date();
-    // let nowadays = new Date(nowadaysString.getFullYear(), nowadaysString.getMonth(), nowadaysString.getDate());
-
-    let yearOld = Math.ceil(Math.abs(nowadays.getTime() - fullBirthDate.getTime()) / (1000 * 3600 * 24));
-
-    yearsLab.textContent = `Полных лет человеку: ${yearOld}`;
 }
