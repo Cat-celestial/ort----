2103db253ca57ae28a80dac101f03e63e771d617
Cat-celestial <kirillkoten@gmail.com>
2024-01-24 12:28:17 +0200

    урок 10 сокращенная

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,16 +35,11 @@ for (let prod of storeObj) {
 let priceIncr = pricesNormalList.slice().sort((a, b) => a - b);
 let priceDecr = priceIncr.slice().reverse();
 
-
-console.log("Цены по порядку", pricesNormalList);
-console.log("Цены по спаданию", priceDecr);
-console.log("Цены по возрастанию", priceIncr);
-
 let incInp = document.querySelector("#increaseInp");
-incInp.addEventListener("click", function () {
-    clothBox.innerHTML = "";
+let decrInp = document.querySelector("#decreaseInp");
 
-    for (let pr of priceIncr) {
+let reSortFunc = function (priceMas) {
+    for (let pr of priceMas) {
         let newTag = document.createElement("div");
         let cutDesc = 100;
         let dots = "...";
@@ -69,36 +64,13 @@ incInp.addEventListener("click", function () {
 
         clothBox.appendChild(newTag);
     }
+}
+incInp.addEventListener("click", function() {
+    clothBox.innerHTML = "";
+    reSortFunc(priceIncr);
 });
 
-let decrInp = document.querySelector("#decreaseInp");
-decrInp.addEventListener("click", function () {
+decrInp.addEventListener("click", function() {
     clothBox.innerHTML = "";
-
-    for (let pr of priceDecr) {
-        let newTag = document.createElement("div");
-        let cutDesc = 100;
-        let dots = "...";
-
-        let item = storeObj[pricesNormalList.indexOf(pr)];
-
-        if (item.title.length > 60) {
-            cutDesc = 60;
-        } else if (item.title.length < 60) {
-            dots = "";
-        }
-
-        newTag.innerHTML = `
-            <div class="clothBox">
-                <img src="${item.image}" height="100px" class="prodImg">
-                <h5 class="clothName">${item.title.slice(0, 60) + dots}</h5>
-                <p class="clothDesc">${item.description.slice(0, cutDesc) + "..."}</p>
-                <h5 class="price">$${item.price}</h5>
-            </div>
-        `;
-
-
-        clothBox.appendChild(newTag);
-    }
-
+    reSortFunc(priceDecr);
 });
