6cb5b6fb64258d7180525a114f79d3593799bc65
Cat-celestial <kirillkoten@gmail.com>
2024-03-12 23:19:38 +0200

    урок 20 не работает сортировка

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,9 +4,12 @@ const prodsObj = await url.json();
 let inrInp = document.getElementById("increaseInp");
 let decrInp = document.getElementById("decreaseInp");
 
+import ProdsBlock from "./components/prodsBlock.js"
+
 createApp({
     setup() {
-        let stafVueObj = prodsObj.map(obj => ({ ...obj, allText: `${obj.title.toLowerCase()} ${obj.description.toLowerCase()}` }));
+        let stafVueObj = prodsObj.map(obj => ({ ...obj, allText: `${obj.title.toLowerCase()} ${obj.description.toLowerCase()}`, selectAmount: 0 }));
+        console.log(stafVueObj)
         let minPrice = ref(9);
         let maxPrice = ref(1000);
         let minPStorage = localStorage.getItem("minPrc");
@@ -14,13 +17,14 @@ createApp({
         if (minPStorage) { minPrice.value = minPStorage; } else { localStorage.setItem("minPrc", minPrice.value); }
         if (maxPStorage) { maxPrice.value = maxPStorage; } else { localStorage.setItem("maxPrc", maxPrice.value); }
 
+        // сохранение статуса сортировки в local storage
         let sortDirect = ref("");
         let sortDStorage = localStorage.getItem("sortDrct");
         if (sortDStorage) {
             sortDirect.value = sortDStorage;
             if (sortDirect.value == "up") {
                 inrInp.setAttribute('checked', 'checked');
-            } else if (sortDirect.value == "down"){
+            } else if (sortDirect.value == "down") {
                 decrInp.setAttribute('checked', 'checked');
             }
         } else { localStorage.setItem("sortDrct", ""); }
@@ -32,7 +36,7 @@ createApp({
             localStorage.setItem("maxPrc", maxPrice.value);
 
             let clothBox = document.querySelector(".mainClothBox");
-            clothBox.innerHTML = "";
+            // clothBox.innerHTML = "";
             let sortedProds = stafVueObj.filter(obj => obj.allText.includes(searchValue.value.trim().toLowerCase()) && obj.price > minPrice.value && obj.price < maxPrice.value);
 
             if (sortDirect.value == "down") {
@@ -41,29 +45,37 @@ createApp({
                 sortedProds = sortedProds.sort((a, b) => a.price - b.price);
             }
 
-            for (let item of sortedProds) {
-                let newTag = document.createElement("div");
-                let cutDesc = 100;
-                let dots = "...";
+            clothBox.innerHTML = `
+                <prods-block
+                    v-for="prod in ${sortedProds}"
+                    :prods-info="prod"
+                ></prods-block>
+            `
 
-                if (item.title.length > 60) {
-                    cutDesc = 60;
-                } else if (item.title.length < 60) {
-                    dots = "";
-                }
+            // for (let item of sortedProds) {
+            //     let newTag = document.createElement("div");
+            //     let cutDesc = 100;
+            //     let dots = "...";
 
-                newTag.innerHTML = `
-                    <div class="clothBox">
-                        <img src="${item.image}" height="100px" class="prodImg">
-                        <h5 class="clothName">${item.title.slice(0, 60) + dots}</h5>
-                        <p class="clothDesc">${item.description.slice(0, cutDesc) + "..."}</p>
-                        <h5 class="price">$${item.price}</h5>
-                    </div>
-                `;
+            //     if (item.title.length > 60) {
+            //         cutDesc = 60;
+            //     } else if (item.title.length < 60) {
+            //         dots = "";
+            //     }
+
+            //     newTag.innerHTML = `
+            //         <div class="clothBox">
+            //             <img src="${item.image}" height="100px" class="prodImg">
+            //             <h5 class="clothName">${item.title.slice(0, 60) + dots}</h5>
+            //             <p class="clothDesc">${item.description.slice(0, cutDesc) + "..."}</p>
+            //             <h5 class="price">$${item.price}</h5>
+            //             <prods-block></prods-block>
+            //         </div>
+            //     `;
 
 
-                clothBox.appendChild(newTag);
-            }
+            //     clothBox.appendChild(newTag);
+            // }
         };
 
         onMounted(() => {
@@ -91,5 +103,8 @@ createApp({
             render,
             increaseFunc, decreaseFunc
         }
+    },
+    components: {
+        ProdsBlock
     }
 }).mount("#app");
---
+++ assets/js/components/prodsBlock.js
@@ -0,0 +1,24 @@
+import { computed } from "vue";
+
+export default {
+    props: ["prodsInfo"],
+    setup(props) {
+        return {
+            props
+        }
+    },
+    template: `
+        <div class="clothBox">
+            <img :src="props.prodsInfo.image" height="100px" class="prodImg">
+            <h5 class="clothName">{{props.prodsInfo.title.slice(0, 60) + "..."}}</h5>
+            <p class="clothDesc">{{props.prodsInfo.description.slice(0, 100) + "..."}}</p>
+            <h5 class="price">&#36;{{props.prodsInfo.price}}</h5>
+            
+            <div class="prodAmountBox">
+                <button class="changeAmProdBut">-1</button>
+                <p class="currProdAm">0</p> 
+                <button class="changeAmProdBut">+1</button>
+            </div>
+        </div>
+    `
+}
